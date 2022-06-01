b665cd179b4a96acdb59498ccf22174ca160ee7e
DogRing <aldns232@cau.ac.kr>
2022-06-01 20:50:21 +0900

    dataset to only close

--- pymodule/dataMod.py
+++ pymodule/dataMod.py
@@ -7,13 +7,14 @@ from torch.utils.data import Dataset
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class OHLCDataset(Dataset):
-    def __init__(self, t_df ,pred ,seq_length, ):
-        super(BasicDataset, self).__init__()
+    def __init__(self, t_df ,pred ,seq_length, ohlc=True):
+        super(OHLCDataset, self).__init__()
         self.seq_length = seq_length
         self.start = seq_length*1440
+        self.ohlc = ohlc
         
         self.df_T = torch.FloatTensor(self.sampling_range(t_df, 'T').values).to(device)
-        self.df_10T = torch.FloatTensor(self.sampling_range(t_df, '10T').values).to(device)
+        self.df_10T = torch.FloatTensor(self.sampling_range(t_df, '15T').values).to(device)
         self.df_H = torch.FloatTensor(self.sampling_range(t_df, '60T').values).to(device)
         self.df_D = torch.FloatTensor(self.sampling_range(t_df, '1440T').values).to(device)
         
@@ -39,14 +40,17 @@ class OHLCDataset(Dataset):
 
 
     def sampling_range(self, df, T_range):
-        df_sample = pd.DataFrame(df['open'].resample(T_range).first())
-        df_sample['high'] = df['high'].resample(T_range).max()
-        df_sample['volume'] = df['volume'].resample(T_range).sum()
-        df_sample['low'] = df['low'].resample(T_range).min()
-        df_sample['close'] = df['close'].resample(T_range).last()
-        for i in ['open','high','low','close']:
-            df_sample[i] = df_sample[i] * 100 / df.close
+        if self.ohlc:
+            df_sample = pd.DataFrame(df['open'].resample(T_range).first())
+            df_sample['high'] = df['high'].resample(T_range).max()
+            df_sample['volume'] = df['volume'].resample(T_range).sum()
+            df_sample['low'] = df['low'].resample(T_range).min()
+            for i in ['open','high','low']:
+                df_sample[i] = df_sample[i] * 100 / df.close
         
+        df_sample['close'] = df['close'].resample(T_range).last()
+        df_sample['close'] = df_sample['close'] * 100 / df.close
+
         for i in df_sample:
             df_sample[i] = df_sample[i] / df_sample[i].abs().max()
         
