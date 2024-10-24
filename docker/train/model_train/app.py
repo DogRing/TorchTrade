2b772987ecb0b00e28ec3ef93121d02332ad997f
DogRing <changh232@naver.com>
2024-10-24 13:45:28 +0900

    time as KST

--- docker/train/model_train/app.py
+++ docker/train/model_train/app.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from tqdm import tqdm
 import json
 import time
 import sys
@@ -14,6 +13,7 @@ num_epochs = int(os.environ.get('EPOCHES','50'))
 tqdm_refresh = int(os.environ.get('TQDM_REFRESH','1000'))
 param_path = os.environ.get('RESULT_PATH','/model/results/result.pt')
 config_file = os.environ.get('MODEL_CONFIG','/model/model.json')
+kst_offset = 9 * 3600
 
 device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
@@ -28,13 +28,12 @@ if os.path.exists(param_path)==True:
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-start=time.time()
+start=time.time() + kst_offset
 print(f'Start At {time.strftime("%m-%d %H:%M:%S",time.localtime(start))}')
 for epoch in range(num_epochs):
     model.train()
     train_loss = 0.0
-    train_pbar = tqdm(total=len(train_loader),desc=f'Epoch{epoch+1}/{num_epochs} [Train]',mininterval=20,position=0,leave=False)
-    for i, (input_tensor, target_tensor) in enumerate(train_loader):
+    for input_tensor, target_tensor in train_loader:
         input_tensor = input_tensor.to(device)
         target_tensor = target_tensor.to(device)
         outputs = model(input_tensor)
@@ -43,10 +42,6 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         train_loss += loss.item()
-        if (i + 1) % tqdm_refresh == 0 or (i + 1) == len(train_loader):
-            train_pbar.update(tqdm_refresh if i != len(train_loader)-1 else len(train_loader) % tqdm_refresh)
-            train_pbar.set_description(f'Epoch {epoch+1}/{num_epochs} [Train] loss: {train_loss / (i + 1):.4f}')
-    train_pbar.close()
     avg_train_loss = train_loss / len(train_loader)
     model.eval()
     test_loss = 0.0
@@ -58,7 +53,15 @@ for epoch in range(num_epochs):
             loss = criterion(outputs, target_tensor)
             test_loss += loss.item()
     avg_test_loss = test_loss / len(test_loader)
-    print(f'Epoch [{epoch+1}/{num_epochs}] {((time.time()-start)/60):.2f}M, Train Loss: {avg_train_loss:.4f}, Test Loss: {avg_test_loss:.4f}')
+    spend = time.time() - start + kst_offset
+    time_per_epoch = spend / (epoch + 1)
+    next_epoch_end_time = start + (epoch + 2) * time_per_epoch
+    all_epoch_end_time = start + num_epochs * time_per_epoch
+    print(f'Epoch [{epoch+1}/{num_epochs}] '
+            f'Loss (Train|Test): {avg_train_loss:.4f} | {avg_test_loss:.4f}, '
+            f'Next: {time.strftime("%H:%M",time.localtime(next_epoch_end_time))}, '
+            f'End: {time.strftime("%H:%M",time.localtime(all_epoch_end_time))} '
+            f'Spend: {((spend)/60):.2f}M')
     torch.save(model.state_dict(),param_path)
-end=time.time()
+end=time.time() + kst_offset
 print(f'End At {time.strftime("%m-%d %H:%M:%S",time.localtime(end))}, Mean per epoch {((end-start)/60/num_epochs):.2f}')
